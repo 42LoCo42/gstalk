9c22950aefb75406d1588077fd05da3bb01972a8
Leon Schumacher <leonsch@protonmail.com>
2026-03-16 07:45:07 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs.lib.fileset) toSource unions;
+        stdenv = pkgs.gccStdenv; # pkgs.clangStdenv;
+      in
+      rec {
+        packages.default = stdenv.mkDerivation {
+          pname = "gstalk";
+          version = "0.0.1";
+
+          src = toSource {
+            root = ./.;
+            fileset = unions [
+              ./meson.build
+              ./src
+            ];
+          };
+
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+          ];
+
+          buildInputs = with pkgs; [
+          ];
+
+          mesonBuildType = "release";
+          mesonFlags = [ "--werror" ];
+        };
+
+        devShells.default = (pkgs.mkShell.override {
+          inherit stdenv;
+        }) {
+          inputsFrom = [ packages.default ];
+          packages = with pkgs; [
+            clang-tools
+            just
+          ];
+        };
+      });
+}
